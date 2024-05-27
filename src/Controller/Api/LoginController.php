987bc8c9af019e7aba1f90d7ce5bf340589fab9f
Andrzej Szelka <szelkaandrzej@gmail.com>
2024-05-27 15:45:19 +0200

    Add e.g. controller to api, add limiter

---
+++ src/Controller/Api/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class LoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser()] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = ''; // somehow create an API token for $user
+
+        return $this->json([
+            'user'  => $user->getUserIdentifier(),
+            'token' => $token
+        ]);
+    }
+}
